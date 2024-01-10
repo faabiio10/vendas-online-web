4a9678f089c5641bae5eaa59167216d4a60385de
faabiio10 <31457514+faabiio10@users.noreply.github.com>
2024-01-10 20:29:46 -0300

    feat: change layout and create button component

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,5 +1,6 @@
+import Button from "../../../shared/buttons/buttons/Button";
 import Input from "../../../shared/inputs/input/input";
-import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage } from "../styles/loginScreen.styles";
+import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage, TitleLogin } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
     return (
@@ -8,8 +9,10 @@ const LoginScreen = () => {
             <ContainerLogin>
                 <LimitedContainer>
                     <LogoImage src="./logo.png"/>
+                    <TitleLogin level={2} type="secondary">LOGIN</TitleLogin>
                     <Input title="Usuário" type="text"/>
                     <Input title="Senha" type="password"/>
+                    <Button type="primary" margin="64px 0px 16px 0px">ENTRAR</Button>
                 </LimitedContainer>
             </ContainerLogin>
         </ContainerLoginScreen>
--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { Space, Typography } from 'antd';
+
+const { Title, Link } = Typography;
 
 export const ContainerLoginScreen = styled.div`
     width: 100%;
@@ -17,7 +20,10 @@ export const BackgroundImage = styled.img`
 `
 
 export const LogoImage = styled.img`
-
+    width: 202px;
+`
+export const TitleLogin = styled(Title)`
+    color: #006397;
 `
 
 export const ContainerLogin = styled.div`
@@ -35,5 +41,6 @@ export const LimitedContainer = styled.div`
     max-width: 498px;
     display: flex;
     justify-content: center;
+    align-items: center;
     flex-direction: column;
 `
