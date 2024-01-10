c3f7e81575e321e4eff4deb518f5107a9f25d99d
faabiio10 <31457514+faabiio10@users.noreply.github.com>
2024-01-10 19:54:08 -0300

    feat: create first global component

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,14 +1,19 @@
+import Input from "../../../shared/inputs/input/input";
 import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
-    return <ContainerLoginScreen>
-        <BackgroundImage src="./background.png"/>
-        <ContainerLogin>
-            <LimitedContainer>
-                <LogoImage src="./logo.png"/>
-            </LimitedContainer>
-        </ContainerLogin>
-    </ContainerLoginScreen>
+    return (
+        <ContainerLoginScreen>
+            <BackgroundImage src="./background.png"/>
+            <ContainerLogin>
+                <LimitedContainer>
+                    <LogoImage src="./logo.png"/>
+                    <Input title="Usuário" type="text"/>
+                    <Input title="Senha" type="password"/>
+                </LimitedContainer>
+            </ContainerLogin>
+        </ContainerLoginScreen>
+    )
 }
 
 export default LoginScreen;
