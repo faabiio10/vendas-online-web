6eeaa7cbf900da2cddf722287becdc59ea386493
faabiio10 <31457514+faabiio10@users.noreply.github.com>
2024-01-11 11:24:27 -0300

    feat: insert react-router

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,23 @@ import './main.css';
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouteObject, RouterProvider } from "react-router-dom";
+import type { Router as RemixRouter } from '@remix-run/router'
 
-import App from './App.tsx'
+import { loginRoutes } from './modules/login/routes';
+
+const mainRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <div>Tela Princial</div>,
+    errorElement: <div>Página não encontrada</div>
+  }
+];
+
+const router: RemixRouter = createBrowserRouter([ ...mainRoutes, ...loginRoutes]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/modules/login/routes.tsx
@@ -0,0 +1,11 @@
+import { RouteObject } from "react-router-dom";
+
+import LoginScreen from ".";
+
+
+export const loginRoutes: RouteObject[] = [
+    {
+      path: "/login",
+      element: <LoginScreen />
+    }
+];
